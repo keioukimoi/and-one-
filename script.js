a6044c2783ea133fd4ec02b7a0640e0ffbcd8711
shindoidesu <yuu.ikeiii@gmail.com>
2026-06-05 20:06:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const markerObjects = MARKERS.map(point => {
+const markerObjects = markers.map(point => {
   const markers = new google.maps.Marker({
     position: { lat: point.lat, lng: point.lng },
     map: map,
