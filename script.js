39cdceb99a5df54dd71a6942199d83381adc2ad1
shindoidesu <yuu.ikeiii@gmail.com>
2026-06-19 19:04:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
 let map;
-function updateMarkers() {
-  const zoom = map.getZoom();
-  const popcornSize = zoom >= 18 ? 60 : 40;  // ← ズームに応じてサイズ決定
+function initializeMarkers(mapInstance) {
+  map = mapInstance;
 
-  markerObjects.forEach(({ marker, category }) => {
-    if (zoom >= 16.5) {
-      marker.setVisible(true);
-    } else {
-      marker.setVisible(false);
-    }
-
-    // ポップコーンのアイコンサイズを変更
-    if (category === "popcorn") {
-      marker.setIcon({
-        url: "cut.png",
-        scaledSize: new google.maps.Size(popcornSize, popcornSize),
-        anchor: new google.maps.Point(popcornSize / 2, popcornSize)
-      });
-    }
-  });
-}
+  const icons = {
+    popcorn: {
+  url: "cut.png",
+  scaledSize: new google.maps.Size(60, 60)
+},
+    food:    "https://maps.google.com/mapfiles/ms/icons/orange-dot.png",
+    toilet:  "https://maps.google.com/mapfiles/ms/icons/blue-dot.png",
+  };
 
   const markerObjects = markers.map(point => {
     const marker = new google.maps.Marker({
