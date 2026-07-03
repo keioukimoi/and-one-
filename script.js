67c546d03202f85a0e92798c694cc9688c77377b
shindoidesu <yuu.ikeiii@gmail.com>
2026-07-03 19:38:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function initializeMarkers(mapInstance) {
     },
     food:    "https://maps.google.com/mapfiles/ms/icons/orange-dot.png",
     toilet: {
-      url: "toilet.PNG",
+      url: "toilet.png",
       scaledSize: new google.maps.Size(60, 60)
     },
   };
