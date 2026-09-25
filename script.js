72e15faf3fbb9a3d9e400db78ec741cbe861c0fd
shindoidesu <yuu.ikeiii@gmail.com>
2026-09-25 18:42:44 +0900

    Change toilet icon URL to local image

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function initializeMarkers(mapInstance) {
       scaledSize: new google.maps.Size(60, 60)
     },
     food:    "https://maps.google.com/mapfiles/ms/icons/orange-dot.png",
-    toilet:  "https://maps.google.com/mapfiles/ms/icons/blue-dot.png",
+    toilet:  "toilet.png",
   };
   const markerObjects = markers.map(point => {
     const marker = new google.maps.Marker({
